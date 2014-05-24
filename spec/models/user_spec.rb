71be7d36f7075185c17f41aef7dd016f31061b3b
Michal Bryxí <michal.bryxi@gmail.com>
2014-05-24 23:38:43 +0200

    Fixed test to cover age

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,6 @@ require 'spec_helper'
 describe User, :type => :model do
   it { should respond_to(:first_name) }
   it { should respond_to(:last_name) }
+  it { should respond_to(:age) }
   it { should respond_to(:active) }
 end
