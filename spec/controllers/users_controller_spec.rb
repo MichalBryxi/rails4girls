0d90c9e9832e65e6c2fd4e9d54d01984d34aa492
Michal Bryxí <michal.bryxi@gmail.com>
2014-05-26 16:11:19 +0200

    Spec for saving e-mail

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -23,7 +23,7 @@ describe UsersController, :type => :controller do
   # This should return the minimal set of attributes required to create a valid
   # User. As you add validations to User, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "first_name" => "MyString" } }
+  let(:valid_attributes) { { "first_name" => "MyString", "email" => "john.doe@gmail.com" } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,5 @@ describe User, :type => :model do
   it { should respond_to(:last_name) }
   it { should respond_to(:age) }
   it { should respond_to(:active) }
+  it { should respond_to(:email) }
 end
