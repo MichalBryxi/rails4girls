fde9c38d183e22c514e35453720a40824fb38d05
Michal Bryxí <michal.bryxi@gmail.com>
2014-06-05 10:33:55 +0200

    Implemented hw_03

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  validates :first_name, length: { in: 3..15 }
+  validates :last_name, length: { minimum: 3 }
+  validates :age, :inclusion => 13..99
+  validates :email, uniqueness: true
 end
