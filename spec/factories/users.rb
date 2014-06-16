49845e893fcc2a6b2d4a82daa934bc92d1f4829b
Michal Bryxí <michal.bryxi@gmail.com>
2014-06-16 07:55:45 +0200

    hw_04 might be completed

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,19 @@
 FactoryGirl.define do
   factory :user do
+    trait :john do
+      first_name "John"
+      last_name "Doe"
+      age 20
+      email "john.doe@example.com"
+    end
+
+    trait :jane do
+      first_name "Jane"
+      last_name "Doe"
+      age 22
+      email "jane.doe@example.com"
+    end
+
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     age 27
--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -2,31 +2,34 @@ require 'spec_helper'
 
 describe "users/index", :type => :view do
   before(:each) do
-    assign(:users, [
-      User.create!(
-        :first_name => "First Name",
-        :last_name => "Last Name",
-        :age => 20,
-        :active => false,
-        :email => 'john.doe@gmail.com'
-      ),
-      User.create!(
-        :first_name => "First Name",
-        :last_name => "Last Name",
-        :age => 20,
-        :active => false,
-        :email => 'jane.doe@gmail.com'
-      )
-    ])
+    @users = [
+      FactoryGirl.create(:user, :john),
+      FactoryGirl.create(:user, :jane)
+    ]
+
+    4.times { FactoryGirl.create(:post, user: @users[0]) }
+    5.times { FactoryGirl.create(:post, user: @users[1]) }
+  end
+
+  it "has header for post count" do
+    render
+    assert_select "tr>th", :text => "Příspěvků".to_s, :count => 1
+  end
+
+  it "has column with post count" do
+    render
+    assert_select "tr>td", :text => 4.to_s, :count => 1
+    assert_select "tr>td", :text => 5.to_s, :count => 1
   end
 
   it "renders a list of users" do
     render
-    assert_select "tr>td", :text => "First Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Last Name".to_s, :count => 2
-    assert_select "tr>td", :text => 20.to_s, :count => 2
-    assert_select "tr>td", :text => false.to_s, :count => 2
-    assert_select "tr>td", :text => "john.doe@gmail.com".to_s, :count => 1
-    assert_select "tr>td", :text => "jane.doe@gmail.com".to_s, :count => 1
+    assert_select "tr>td", :text => "John".to_s, :count => 1
+    assert_select "tr>td", :text => "Jane".to_s, :count => 1
+    assert_select "tr>td", :text => "Doe".to_s, :count => 2
+    assert_select "tr>td", :text => 20.to_s, :count => 1
+    assert_select "tr>td", :text => 22.to_s, :count => 1
+    assert_select "tr>td", :text => "john.doe@example.com".to_s, :count => 1
+    assert_select "tr>td", :text => "jane.doe@example.com".to_s, :count => 1
   end
 end
