91a0af78df211b0df9d35da4e8c8b1627e66a183
Michal Bryxí <michal.bryxi@gmail.com>
2014-06-27 09:52:02 +0200

    hw_05 base

--- spec/views/posts/edit.html.erb_spec.rb
+++ spec/views/posts/edit.html.erb_spec.rb
@@ -2,20 +2,17 @@ require 'spec_helper'
 
 describe "posts/edit", :type => :view do
   before(:each) do
-    @post = assign(:post, Post.create!(
-      :body => "MyString",
-      :user_id => 1
-    ))
+    @post = FactoryGirl.create(:post)
+    @user = FactoryGirl.create(:user)
   end
 
   it "renders the edit post form" do
     render
 
     assert_select "form[action=?][method=?]", post_path(@post), "post" do
-
       assert_select "input#post_body[name=?]", "post[body]"
-
-      assert_select "input#post_user_id[name=?]", "post[user_id]"
+      assert_select "select#post_user_id[name=?]", "post[user_id]"
+      assert_select "select#post_user_id option[value=1]", @user.name
     end
   end
 end
--- spec/views/posts/new.html.erb_spec.rb
+++ spec/views/posts/new.html.erb_spec.rb
@@ -12,10 +12,8 @@ describe "posts/new", :type => :view do
     render
 
     assert_select "form[action=?][method=?]", posts_path, "post" do
-
       assert_select "input#post_body[name=?]", "post[body]"
-
-      assert_select "input#post_user_id[name=?]", "post[user_id]"
+      assert_select "select#post_user_id[name=?]", "post[user_id]"
     end
   end
 end
